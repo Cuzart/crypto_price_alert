2e9d05a89e470e43ea1ace46c467e2bc5de8eb82
ch148 <ch148@hdm-stuttgart.de>
2021-05-06 17:18:25 +0200

    🎨  catch errors and improve dialog

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from functions import check_price_below, check_price_above, check_is_trending, g
 from pycoingecko import CoinGeckoAPI
 
 import time
+import requests
+
 
 cg = CoinGeckoAPI()
 
@@ -14,33 +16,42 @@ price_goal_below = 0
 asset_short = input("Enter shortform of the asset you want to watch: ")
 asset_id = get_coingecko_id(asset_short)
 
+asset = cg.get_price(ids=asset_id, vs_currencies='eur', include_24hr_change="true", include_market_cap="true")
+asset_price = round(asset[asset_id]['eur'], 4)
+print("Great choice {} is currently at {}.".format(asset_short, asset_price))
+
+
 in_above = input("Do you want to get notified when your asset is above your goal ? (y/n): ")
-in_below = input("Do you want to get notified when your asset is below your goal ? (y/n): ")
-
-
 if in_above == "y":
     price_goal_above = float(input("Enter your upper price goal: "))
 
+in_below = input("Do you want to get notified when your asset is below your goal ? (y/n): ")
 if in_below == "y":
     price_goal_below = float(input("Enter your lower price goal: "))
 
 
 while True:
-    check_is_trending(watched_assets)
+    try:
+        check_is_trending(watched_assets)
 
-    # get the data from the api
-    asset = cg.get_price(ids=asset_id, vs_currencies='eur', include_24hr_change="true", include_market_cap="true")
+        # get the data from the api
+        asset = cg.get_price(ids=asset_id, vs_currencies='eur', include_24hr_change="true", include_market_cap="true")
 
-    asset_price = round(asset[asset_id]['eur'], 4)
-    asset_24h_change = round(asset[asset_id]['eur_24h_change'], 2)
-    asset_mcap = round(asset[asset_id]['eur_market_cap'])
-    print("Current price of {} is at {}€ with a {}% change in 24h and a market cap of {:,}€."
-          .format(asset_id, asset_price, asset_24h_change, asset_mcap))
+        asset_price = round(asset[asset_id]['eur'], 4)
+        asset_24h_change = round(asset[asset_id]['eur_24h_change'], 2)
+        asset_mcap = round(asset[asset_id]['eur_market_cap'])
+        print("Current price of {} is at {}€ with a {}% change in 24h and a market cap of {:,}€."
+              .format(asset_id, asset_price, asset_24h_change, asset_mcap))
 
-    if in_above == "y":
-        check_price_above(asset_price, price_goal_above, asset_short.upper())
+        if in_above == "y":
+            check_price_above(asset_price, price_goal_above, asset_short.upper())
 
-    if in_below == "y":
-        check_price_below(asset_price, price_goal_below, asset_short.upper())
+        if in_below == "y":
+            check_price_below(asset_price, price_goal_below, asset_short.upper())
+
+    except requests.exceptions.Timeout:
+        print("Timeout occured")
+    except requests.exceptions.ConnectionError:
+        print("Connection Error occured")
 
     time.sleep(int(getenv('TIME_INTERVAL')))

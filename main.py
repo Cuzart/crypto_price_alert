7cea4789cecced06ab7af7634592cf9890a9f1bc
Chris <ch148@hdm-stuttgart.de>
2022-03-24 18:37:01 +0100

    ✨ only send mail if new key in notifications dict

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from decouple import config as getenv
 from functions import  get_trending_assets, check_price_action, send_email
 from pycoingecko import CoinGeckoAPI
-
 import time
 import requests
-
-
 
 watched_assets = getenv('WATCHED_ASSETS')
 notifications = {}
@@ -16,26 +13,23 @@ iterations_until_refresh = seconds_in_day / int(getenv('TIME_INTERVAL'))
 
 while True:
     try:
-        count_notifications = len(notifications)
+        old_notifications = notifications
         check_price_action(watched_assets, notifications)
 
-        # reset after 24h
+        # reset after 24h = after iteration_until_refresh
         counter += 1
+        if counter % iterations_until_refresh == 0: notifications = {}
         print(counter)
 
-        if counter % iterations_until_refresh == 0: notifications = {}
-
-
         # only if there is a new notification added
-        if len(notifications) > count_notifications:
+        if any(x not in old_notifications for x in notifications):
           message = "🚨 Price Alerts 🚨 \n\n"
           for note in notifications:
             message += "Current price of {0[0]} is at {0[1]}€ with a {0[2]}% change in 24h.".format(notifications[note]) + '\n\n'
- 
+
           # add my assets that are trending right now
           message += get_trending_assets(watched_assets)
           send_email("New abnormal price actions! 📈 🥳 ", message)
-
 
     except requests.exceptions.Timeout:
         print("Timeout occured")

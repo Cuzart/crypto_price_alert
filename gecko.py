329d68058436ab34df09ab73124128bd66fb61e1
Chris <ch148@hdm-stuttgart.de>
2022-04-07 23:02:06 +0200

    🎨  refactor to classes and improve encapsulation

---
+++ gecko.py
@@ -0,0 +1,65 @@
+from decouple import config as getenv
+from pycoingecko import CoinGeckoAPI
+import json
+
+
+class Gecko:
+    def __init__(self):
+        self.asset_list = getenv("WATCHED_ASSETS")
+        self.cg = CoinGeckoAPI()
+        self.all_coins = self.cg.get_coins_list()
+        self.notifications = {}
+
+    # check if trending asset is in asset list
+    def get_trending_assets(self):
+        header = "⚡ Trending on Coingecko ⚡ \n\n"
+        trending = []
+
+        for coin in self.cg.get_search_trending()["coins"]:
+            if coin['item']['symbol'] in self.asset_list:
+                trending.append(coin['item']['symbol'])
+
+        # only if trending assets are in asset_list
+        return header + ", ".join(trending) if trending else ""
+
+    # get list of trending coins
+    def get_trending(self):
+        trending = []
+        for entry in self.cg.get_search_trending()["coins"]:
+            trending.append("{} ({})".format(entry['item']['name'], entry['item']['symbol']))
+        return "\n".join(trending).rstrip("\n")
+
+    # check if price of assets in list are up/down by the limit in their 24h change
+    def check_price_action(self):
+        for index, asset in enumerate(json.loads(self.asset_list)):
+            asset_data = self.get_asset_data(asset)
+            asset_price = asset_data[1]
+            asset_24h_change = asset_data[2]
+
+            # check for abnormal price activity on 24h change
+            if asset_24h_change < int(getenv("LOWER_LIMIT")) or asset_24h_change > int(getenv("UPPER_LIMIT")):
+                self.notifications[index] = (asset, asset_price, asset_24h_change)
+            else:
+                # delete entry if it falls below/above the limit
+                if index in self.notifications:
+                    del self.notifications[index]
+
+        print(self.notifications)
+
+    # get asset with price and 24h change from api
+    def get_asset_data(self, asset_shortform):
+        asset_id = self.get_coingecko_id(asset_shortform)
+        data = self.cg.get_price(ids=asset_id, vs_currencies='eur', include_24hr_change="true")
+
+        asset_price = round(data[asset_id]['eur'], 4)
+        asset_24h_change = round(data[asset_id]['eur_24h_change'], 2)
+
+        return (asset_shortform.upper(), asset_price, asset_24h_change)
+
+    # returns the correct coingecko id of an asset by it's shortform e.g. bitcoin for BTC
+    def get_coingecko_id(self, asset_shortform):
+        for coin in self.all_coins:
+            if coin['symbol'] == asset_shortform.lower():
+                return coin['id']
+
+        print("Sorry did not found a match for your asset {}.".format(asset_shortform))
--- main.py
+++ main.py
@@ -1,43 +1,53 @@
-from decouple import config as getenv
-from functions import  get_trending_assets, check_price_action, send_telegram, send_email
-from pycoingecko import CoinGeckoAPI
+from threading import Thread
 import time
+from telegram_bot import TelegramBot, send_message
+from decouple import config as getenv
+from gecko import Gecko
 import requests
 
-watched_assets = getenv('WATCHED_ASSETS')
-notifications = {}
 
-counter = 0
-seconds_in_day = 24 * 60 * 60
-iterations_until_refresh = seconds_in_day / int(getenv('TIME_INTERVAL'))
+def start_alert():
+    gecko = Gecko()
 
-while True:
-    try:
-        old_notifications = dict(notifications)
-        check_price_action(watched_assets, notifications)
+    counter = 0
+    seconds_in_day = 24 * 60 * 60
+    iterations_until_refresh = seconds_in_day / int(getenv('TIME_INTERVAL'))
 
-        # reset after 24h = after iteration_until_refresh
-        counter += 1
-        if counter % iterations_until_refresh == 0: notifications = {}
-        print(counter)
+    while True:
+        try:
+            old_notifications = dict(gecko.notifications)
+            gecko.check_price_action()
 
-        # only if there is a new notification added
-        if any(x not in old_notifications for x in notifications):
-          message = "New abnormal price actions! 📈 🥳\n\n"
-          message += "🚨 Price Alerts 🚨 \n\n"
-          for note in notifications:
-            message += "Current price of {0[0]} is at {0[1]}€ with a {0[2]}% change in 24h.".format(notifications[note]) + '\n\n'
+            # reset after 24h = after iterations_until_refresh
+            counter += 1
+            if counter % iterations_until_refresh == 0: gecko.notifications = {}
+            print(counter)
 
-          # add assets that are trending right now
-          message += get_trending_assets(watched_assets)
+            # only if there is a new notification added
+            if any(x not in old_notifications for x in gecko.notifications):
+                message = "New abnormal price actions! 📈 🥳\n\n"
+                message += "🚨 Price Alerts 🚨 \n\n"
+                for note in gecko.notifications:
+                    message += "{0[0]} is at {0[1]}€ with a {0[2]}% change in 24h."\
+                                 .format(gecko.notifications[note]) + '\n\n'
 
-          # uncomment for sending email notifications
-          # send_email("New abnormal price actions! 📈 🥳", message)
-          send_telegram(message)
+                # add assets that are trending right now
+                message += gecko.get_trending_assets()
 
-    except requests.exceptions.Timeout:
-        print("Timeout occured")
-    except requests.exceptions.ConnectionError:
-        print("Connection Error occured")
+                # uncomment for sending email notifications
+                # send_email("New abnormal price actions! 📈 🥳", message)
+                send_message(message)
 
-    time.sleep(int(getenv('TIME_INTERVAL')))
+        except requests.exceptions.Timeout:
+              print("Timeout occured")
+        except requests.exceptions.ConnectionError:
+              print("Connection Error occured")
+
+        time.sleep(int(getenv('TIME_INTERVAL')))
+
+alert_thread = Thread(target=start_alert)
+alert_thread.start()
+TelegramBot()
+
+
+
